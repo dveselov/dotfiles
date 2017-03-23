13e4cb9bb66cae28d9b1f10ff8e3d149c6542940
Dmitry Veselov <d.a.veselov@yandex.ru>
2017-03-24 02:15:42 +0300

    Replace i3blocks with yags

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+
 # Install i3 configs
 ln -s `pwd`/i3/config ~/.config/i3/config
+
+# Install statusline generator
+go get github.com/pltanton/yags
+
+# Install statusline configs
+mkdir ~/.config/yags
+ln -s `pwd`/yags/config.yml ~/.config/yags/config.yml
+
