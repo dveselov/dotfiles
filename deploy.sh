8cb01912bf1c20b0ed45ff54c4f263c17d9aef6c
Dima Veselov <d.a.veselov@yandex.ru>
2018-03-30 15:59:32 +0300

    Replaced i3 with sway

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-# Install i3 configs
-mkdir -p ~/.config/i3/
-ln -sf `pwd`/i3/config ~/.config/i3/config
+# Install Sway configs
+mkdir -p ~/.config/sway/
+ln -sf `pwd`/sway/config ~/.config/sway/config
 
-# Install polybar configs
-mkdir -p ~/.config/polybar
-ln -sf `pwd`/polybar/launch.sh ~/.config/polybar/launch.sh
-ln -sf `pwd`/polybar/config ~/.config/polybar/config
